523dc733d62821f73d9ecbd113590df5702eea3c
wangkunlun <wangkunlun@yuanfudao.com>
2023-04-10 16:20:32 +0800

    FEAT: mini & tab

--- src/js/tiny.react.js
+++ src/js/tiny.react.js
@@ -275,10 +275,10 @@ const Didact = {
 };
 
 /** @jsx Didact.createElement */
-function Counter() {
-  const [state, setState] = Didact.useState(1);
-  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
-}
-const element = <Counter />;
-const container = document.getElementById("root");
-Didact.render(element, container);
+// function Counter() {
+//   const [state, setState] = Didact.useState(1);
+//   return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+// }
+// const element = <Counter />;
+// const container = document.getElementById("root");
+// Didact.render(element, container);
